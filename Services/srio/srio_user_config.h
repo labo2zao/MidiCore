e4c4b212cd5d3eeede111d2c8b70b6dc82e535ee
Ewen d'Aviau <ewen.daviau@gmail.com>
2026-01-18 16:53:29 +0100

    Revert "Merge branch 'copilot/modify-startdefaulttask-for-testing' of https://github.com/labodezao/MidiCore into copilot/modify-startdefaulttask-for-testing"
    This reverts commit 49da68094e772f8c5cd9506f48290406f4edeb84, reversing
    changes made to 79e0b2fba34b9e4810c51ff2baf7bfeac8d2f2b7.

--- Services/srio/srio_user_config.h
+++ Services/srio/srio_user_config.h
@@ -1,16 +1,15 @@
 #pragma once
 
-// SRIO SPI wiring for MBHP_CORE_STM32F4-style pins (MIOS32-compatible).
+// SRIO SPI wiring for MBHP_CORE_STM32F4-style pins.
 //
 // Enable by adding compiler symbol: SRIO_ENABLE
 //
 // IMPORTANT: MIOS32 SPI numbering is different from STM32 SPI numbering!
 // SRIO uses MIOS32_SPI1 which maps to:
 //   - SPI2 for SCK/MISO/MOSI: PB13(SCK) PB14(MISO) PB15(MOSI)
-//   - RC1 (74HC595 RCLK for DOUT): PA15 (SRIO_RC1_Pin in main.h)
-//   - RC2 (74HC165 /PL for DIN): PE1 (SRIO_RC2_Pin in main.h)
+//   - RC1 (74HC595 RCLK for DOUT): PB12 (OLED_CS_Pin in main.h - rename OLED_CS in CubeMX for actual SSD display)
+//   - RC2 (74HC165 /PL for DIN): PD10 (MIOS_SPI1_RC2_Pin in main.h)
 //
-// This matches the standard MIOS32/MBHP_CORE_STM32F4 pin assignment.
 // This is different from AINSER64 which uses MIOS32_SPI0 (STM32 SPI3, RC=PA14)
 
 #include "main.h"
@@ -22,13 +21,14 @@
 extern SPI_HandleTypeDef hspi2;
 #define SRIO_SPI_HANDLE (&hspi2)
 
-// 74HC165 /PL uses RC2 (PE1 = SRIO_RC2_Pin as defined in main.h per MIOS32 standard)
-#define SRIO_DIN_PL_PORT SRIO_RC2_GPIO_Port
-#define SRIO_DIN_PL_PIN  SRIO_RC2_Pin
+// 74HC165 /PL uses RC2 (PD10 = MIOS_SPI1_RC2 as defined in main.h)
+#define SRIO_DIN_PL_PORT MIOS_SPI1_RC2_GPIO_Port
+#define SRIO_DIN_PL_PIN  MIOS_SPI1_RC2_Pin
 
-// 74HC595 RCLK uses RC1 (PA15 = SRIO_RC1_Pin as defined in main.h per MIOS32 standard)
-#define SRIO_DOUT_RCLK_PORT SRIO_RC1_GPIO_Port
-#define SRIO_DOUT_RCLK_PIN  SRIO_RC1_Pin
+// 74HC595 RCLK uses RC1 (PB12 = OLED_CS_Pin in main.h - to be MIOS32 compatible)
+// Note: Rename OLED_CS in CubeMX to a different pin for actual SSD OLED display
+#define SRIO_DOUT_RCLK_PORT OLED_CS_GPIO_Port
+#define SRIO_DOUT_RCLK_PIN  OLED_CS_Pin
 
 // 64 inputs / 64 outputs by default (8 bytes each)
 #define SRIO_DIN_BYTES  8
